9d70ae40c3ec43c1c1c303f0474004c419ce3cf0
sir-clinton <122973587+sir-clinton@users.noreply.github.com>
2025-06-03 23:20:13 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
-(async function () {
-    const data = await fetch('https://sir-clinton.github.io/projects/employees.json');
+   
+    men();
+async function men() {
+    const data = await fetch('../employees.json');
     const res = await data.json();
-
     let employees = res;
-    let selectedEmployeeId = employees[0].id;
-    let selectedEmployee = employees;
+    let selectedEmployee;
+    if(!localStorage.getItem('employees')){
+        localStorage.setItem('employees', JSON.stringify(employees))
+    }
+    employees = localStorage.getItem('employees') ? JSON.parse(localStorage.getItem('employees')) : res;
+    let selectedEmployeeId = employees[employees.length-1].id;
+    console.log(typeof selectedEmployeeId)
 
     const employeeList = document.querySelector(
         '.employees_names--list'
@@ -18,12 +24,21 @@
         '.createEmployee'
     );
 
+
+
     const addEmployeeModal = 
         document.querySelector(".addEmployee"); 
     const addEmployeeForm = document.querySelector( 
         ".addEmployee_create"
     ); 
-    renderEmployees();
+
+    window.deleteEmployee = function (ide){
+        if(!confirm(`Would you like to remove "${selectedEmployee.firstName} ${selectedEmployee.lastName}"`)) return;
+        console.log(ide);
+        employees = employees.filter(el => el.id !== selectedEmployee.id);
+        localStorage.setItem('employees', JSON.stringify(employees));
+        renderEmployees();
+    }
 
     createEmployee.addEventListener('click', ()=> {
         addEmployeeModal.style.display = 'flex';
@@ -60,9 +75,14 @@
         empData.imageUrl  = 
             empData.imageUrl || 'gfg.jpeg';
             employees.push(empData);
+            localStorage.setItem('employees', JSON.stringify(employees))
             renderEmployees();
             addEmployeeForm.reset();
             addEmployeeModal.style.display = 'none'
+            const selectedElement = document.getElementsByClassName('selected')[0]
+           if(selectedElement) selectedElement.classList.remove('selected')
+            document.getElementById(empData.id).classList.add('selected')
+          
     })
     //Add employee end
 
@@ -70,10 +90,13 @@
     employeeList.addEventListener('click', (e)=> {
         if(e.target.tagName === 'SPAN' && selectedEmployeeId !== e.target.id) {
             selectedEmployeeId = e.target.id;
+           const selectedElement = document.getElementsByClassName('selected')[0]
+           if(selectedElement) selectedElement.classList.remove('selected')
+            document.getElementById(e.target.id).classList.add('selected')
             employees.forEach(el => {
                 if(selectedEmployeeId == el.id) selectedEmployee = el;
             });
-            renderSingleEmployee();
+                renderSingleEmployee();
         }
         if (e.target.tagName === "I") { 
             employees = employees.filter( 
@@ -81,8 +104,8 @@
                     String(emp.id) !== 
                     e.target.parentNode.id 
             ); 
-
-            renderEmployees();
+            localStorage.setItem('employees', JSON.stringify(employees));
+        renderEmployees();
         }
     });
 
@@ -92,33 +115,105 @@
             const employee = document.createElement('span');
             employee.classList.add('employees_names--item');
             if(selectedEmployeeId === emp.id) {
-                employee.classList.add('selected');
                 selectedEmployee = emp;
+                employee.classList.add('selected')
+
             }
             employee.setAttribute('id', emp.id);
             employee.innerHTML = `${emp.firstName} ${emp.lastName}  
             <i class="employeeDelete">&#10060;</i>`; 
-        employeeList.append(employee); 
+        employeeList.insertBefore(employee, employeeList.firstChild); 
         })
     };
 
+    window.editEmployee = function(id){
+
+        document.getElementById('save').style.display = 'block';
+        document.getElementById('edit').style.display = 'none';
+        document.querySelector('#address').contentEditable = 'true';
+        document.querySelector('#firstName').contentEditable = 'true';
+        document.querySelector('#firstName').focus();
+        document.querySelector('#lastName').contentEditable = 'true';  
+        document.querySelector('#age').contentEditable = 'true';
+        document.querySelector('#email').contentEditable = 'true';
+        document.querySelector('#mobile').contentEditable = 'true';
+        document.querySelector('#dob').contentEditable = 'true';
+        
+        
+    }
+
+    window.saveEmployee = function(id) {
+        document.getElementById('save').style.display = 'none';
+        document.getElementById('edit').style.display = 'block';
+        document.querySelector('#address').contentEditable = 'false';
+        document.querySelector('#email').contentEditable = 'false';
+        document.querySelector('#mobile').contentEditable = 'false';
+         document.querySelector('#firstName').contentEditable = 'false';
+        document.querySelector('#lastName').contentEditable = 'false';  
+       
+        document.querySelector('#dob').contentEditable = 'false';        
+
+                
+        
+        employees.forEach(el =>{
+            if(el.id === id){
+                selectedEmployeeId = id;
+                el.firstName = document.querySelector('#firstName').textContent;
+                el.lastName = document.querySelector('#lastName').textContent;
+
+                el.email = document.querySelector('#email').textContent
+                el.contactNumber = document.querySelector('#mobile').textContent  
+                el.dob = document.querySelector('#dob').textContent
+                el.address = document.querySelector('#address').textContent
+                    }
+        })
+         
+               
+        localStorage.setItem('employees', JSON.stringify(employees));
+        renderEmployees();
+    }
     //rendersing single employee logic -END
     const renderSingleEmployee = () =>{
+        employeeInfo.innerHTML = '';
         //Employee delete logic 
-        
         employeeInfo.innerHTML = `
-         <img src="${selectedEmployee.imageUrl}" /> 
-        <span class="employees__single--heading"> 
-        ${selectedEmployee.firstName} ${selectedEmployee.lastName}  
-            (${selectedEmployee.age}) 
-        </span> 
-        <span>${selectedEmployee.address}</span> 
-        <span>${selectedEmployee.email}</span> 
-        <span>Mobile - ${selectedEmployee.contactNumber}</span> 
-        <span>DOB - ${selectedEmployee.dob}</span> 
-        `;
+        <input type='file' hidden id='chooseImg'>
+        <div class='imgContainer'>
+         <img id=${selectedEmployee.id} style='cursor: pointer' src="${selectedEmployee.imageUrl}" /></div>
+         <div id='options'><button onclick='deleteEmployee(${selectedEmployee.id})'>DELETE</button><button onclick="saveEmployee(${selectedEmployee.id})" id='save' >SAVE</button><button id='edit' onclick="editEmployee(${selectedEmployee.id})">EDIT</button></div>
+        <div id="employee-info" style='text-align: left; display: flex; flex-direction: column;gap: 4px;'> 
+            <span class="employees__single--heading"><span id='firstName'>${selectedEmployee.firstName}</span> <span id='lastName'>${ selectedEmployee.lastName}</span> <span id='age'>(${selectedEmployee.age})</span></span> 
+        <span id='address'>${selectedEmployee.address}</span> 
+        <span><span  id='email'>${selectedEmployee.email}</span></span> 
+        <span>Mobile - <span  id='mobile'> ${selectedEmployee.contactNumber}</span></span> 
+        <span> DOB - <span  id='dob'>${selectedEmployee.dob}</span></span> 
+        </div>`;
 
+        const employeeInfoImg = employeeInfo.querySelector('.imgContainer').addEventListener('click', changeImage);
+        
     }
-    renderSingleEmployee()
-   
-})()
+
+    function changeImage(){
+        let btni = document.querySelector('#chooseImg')
+        btni.onchange = function(event) {
+            const file = event.target.files[0];
+
+            const reader = new FileReader();
+
+            reader.onload = function(el){
+                employeeInfo.querySelector('img').src = el.target.result
+                employees.forEach(employee => {
+                    if(employee.id === Number(employeeInfo.querySelector('img').id)){
+                        employee.imageUrl = el.target.result;
+                    }
+                })
+                localStorage.setItem('employees', JSON.stringify(employees));
+            }
+
+            reader.readAsDataURL(file);
+        }
+        btni.click();
+    }
+    renderEmployees();
+    renderSingleEmployee();
+}
